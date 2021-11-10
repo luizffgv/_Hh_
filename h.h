b393f4e0d492191135b7551ce8856d12a8e8078a
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2021-11-10 13:58:17 -0300

    :tada: Initial code upload

---
+++ h.h
@@ -0,0 +1,245 @@
+// clang-format off
+
+#ifndef H
+#define H H
+
+// --------------------------- Letters (prefix _) --------------------------- >>
+#define _h     a
+#define _H     b
+#define _Hh    c
+#define _HH    d
+#define _Hhh   e
+#define _HhH   f
+#define _HHh   g
+#define _HHH   h
+#define _Hhhh  i
+#define _HhhH  j
+#define _HhHh  k
+#define _HhHH  l
+#define _HHhh  m
+#define _HHhH  n
+#define _HHHh  o
+#define _HHHH  p
+#define _Hhhhh q
+#define _HhhhH r
+#define _HhhHh s
+#define _HhhHH t
+#define _HhHhh u
+#define _HhHhH v
+#define _HhHHh w
+#define _HhHHH x
+#define _HHhhh y
+#define _HHhhH z
+
+// Capitalized (postfix _)
+#define _h_     A
+#define _H_     B
+#define _Hh_    C
+#define _HH_    D
+#define _Hhh_   E
+#define _HhH_   F
+#define _HHh_   G
+#define _HHH_   H
+#define _Hhhh_  I
+#define _HhhH_  J
+#define _HhHh_  K
+#define _HhHH_  L
+#define _HHhh_  M
+#define _HHhH_  N
+#define _HHHh_  O
+#define _HHHH_  P
+#define _Hhhhh_ Q
+#define _HhhhH_ R
+#define _HhhHh_ S
+#define _HhhHH_ T
+#define _HhHhh_ U
+#define _HhHhH_ V
+#define _HhHHh_ W
+#define _HhHHH_ X
+#define _HHhhh_ Y
+#define _HHhhH_ Z
+
+// --------------------------- ASCII (prefix _h_) --------------------------- >>
+#define _h_h "\x00"
+#define _h_H "\x01"
+#define _h_Hh "\x02"
+#define _h_HH "\x03"
+#define _h_Hhh "\x04"
+#define _h_HhH "\x05"
+#define _h_HHh "\x06"
+#define _h_HHH "\x07"
+#define _h_Hhhh "\x08"
+#define _h_HhhH "\x09"
+#define _h_HhHh "\x0A"
+#define _h_HhHH "\x0B"
+#define _h_HHhh "\x0C"
+#define _h_HHhH "\x0D"
+#define _h_HHHh "\x0E"
+#define _h_HHHH "\x0F"
+#define _h_Hhhhh "\x10"
+#define _h_HhhhH "\x11"
+#define _h_HhhHh "\x12"
+#define _h_HhhHH "\x13"
+#define _h_HhHhh "\x14"
+#define _h_HhHhH "\x15"
+#define _h_HhHHh "\x16"
+#define _h_HhHHH "\x17"
+#define _h_HHhhh "\x18"
+#define _h_HHhhH "\x19"
+#define _h_HHhHh "\x1A"
+#define _h_HHhHH "\x1B"
+#define _h_HHHhh "\x1C"
+#define _h_HHHhH "\x1D"
+#define _h_HHHHh "\x1E"
+#define _h_HHHHH "\x1F"
+#define _h_Hhhhhh "\x20"
+#define _h_HhhhhH "\x21"
+#define _h_HhhhHh "\x22"
+#define _h_HhhhHH "\x23"
+#define _h_HhhHhh "\x24"
+#define _h_HhhHhH "\x25"
+#define _h_HhhHHh "\x26"
+#define _h_HhhHHH "\x27"
+#define _h_HhHhhh "\x28"
+#define _h_HhHhhH "\x29"
+#define _h_HhHhHh "\x2A"
+#define _h_HhHhHH "\x2B"
+#define _h_HhHHhh "\x2C"
+#define _h_HhHHhH "\x2D"
+#define _h_HhHHHh "\x2E"
+#define _h_HhHHHH "\x2F"
+#define _h_HHhhhh "\x30"
+#define _h_HHhhhH "\x31"
+#define _h_HHhhHh "\x32"
+#define _h_HHhhHH "\x33"
+#define _h_HHhHhh "\x34"
+#define _h_HHhHhH "\x35"
+#define _h_HHhHHh "\x36"
+#define _h_HHhHHH "\x37"
+#define _h_HHHhhh "\x38"
+#define _h_HHHhhH "\x39"
+#define _h_HHHhHh "\x3A"
+#define _h_HHHhHH "\x3B"
+#define _h_HHHHhh "\x3C"
+#define _h_HHHHhH "\x3D"
+#define _h_HHHHHh "\x3E"
+#define _h_HHHHHH "\x3F"
+#define _h_Hhhhhhh "\x40"
+#define _h_HhhhhhH "\x41"
+#define _h_HhhhhHh "\x42"
+#define _h_HhhhhHH "\x43"
+#define _h_HhhhHhh "\x44"
+#define _h_HhhhHhH "\x45"
+#define _h_HhhhHHh "\x46"
+#define _h_HhhhHHH "\x47"
+#define _h_HhhHhhh "\x48"
+#define _h_HhhHhhH "\x49"
+#define _h_HhhHhHh "\x4A"
+#define _h_HhhHhHH "\x4B"
+#define _h_HhhHHhh "\x4C"
+#define _h_HhhHHhH "\x4D"
+#define _h_HhhHHHh "\x4E"
+#define _h_HhhHHHH "\x4F"
+#define _h_HhHhhhh "\x50"
+#define _h_HhHhhhH "\x51"
+#define _h_HhHhhHh "\x52"
+#define _h_HhHhhHH "\x53"
+#define _h_HhHhHhh "\x54"
+#define _h_HhHhHhH "\x55"
+#define _h_HhHhHHh "\x56"
+#define _h_HhHhHHH "\x57"
+#define _h_HhHHhhh "\x58"
+#define _h_HhHHhhH "\x59"
+#define _h_HhHHhHh "\x5A"
+#define _h_HhHHhHH "\x5B"
+#define _h_HhHHHhh "\x5C"
+#define _h_HhHHHhH "\x5D"
+#define _h_HhHHHHh "\x5E"
+#define _h_HhHHHHH "\x5F"
+#define _h_HHhhhhh "\x60"
+#define _h_HHhhhhH "\x61"
+#define _h_HHhhhHh "\x62"
+#define _h_HHhhhHH "\x63"
+#define _h_HHhhHhh "\x64"
+#define _h_HHhhHhH "\x65"
+#define _h_HHhhHHh "\x66"
+#define _h_HHhhHHH "\x67"
+#define _h_HHhHhhh "\x68"
+#define _h_HHhHhhH "\x69"
+#define _h_HHhHhHh "\x6A"
+#define _h_HHhHhHH "\x6B"
+#define _h_HHhHHhh "\x6C"
+#define _h_HHhHHhH "\x6D"
+#define _h_HHhHHHh "\x6E"
+#define _h_HHhHHHH "\x6F"
+#define _h_HHHhhhh "\x70"
+#define _h_HHHhhhH "\x71"
+#define _h_HHHhhHh "\x72"
+#define _h_HHHhhHH "\x73"
+#define _h_HHHhHhh "\x74"
+#define _h_HHHhHhH "\x75"
+#define _h_HHHhHHh "\x76"
+#define _h_HHHhHHH "\x77"
+#define _h_HHHHhhh "\x78"
+#define _h_HHHHhhH "\x79"
+#define _h_HHHHhHh "\x7A"
+#define _h_HHHHhHH "\x7B"
+#define _h_HHHHHhh "\x7C"
+#define _h_HHHHHhH "\x7D"
+#define _h_HHHHHHh "\x7E"
+
+// ----------------------- Base - 10 digits(prefix h_) ---------------------- >>
+#define h_h    0
+#define h_H    1
+#define h_Hh   2
+#define h_HH   3
+#define h_Hhh  4
+#define h_HhH  5
+#define h_HHh  6
+#define h_HHH  7
+#define h_Hhhh 8
+#define h_HhhH 9
+
+// ---------------------------- Types (prefix H_) --------------------------- >>
+#define H_    void
+#define H_h   char
+#define H_H   int
+#define H_Hh  float
+#define H_HH  double
+#define H_Hhh short
+#define H_HhH long
+#define H_HHh unsigned
+#define H_HHH signed
+
+// ------------------------- Operators (prefix hh_) ------------------------- >>
+#define hh_h +
+#define hh_H -
+// Operators -- and ++ can be made using h(hh_h, hh_h) and h(hh_H, hh_H)
+#define hh_Hh  *
+#define hh_HH  /
+#define hh_Hhh %
+#define hh_HhH =
+
+// ----------------------- Other symbols (prefix hH_) ----------------------- >>
+#define hH_h (
+#define hH_H )
+#define hH_hH ;
+#define hH_hh {
+#define hH_HH }
+#define hH_hhh [
+#define hH_HHH [
+
+// ----------------------- Utility macros (prefix HH_) ---------------------- >>
+
+// Identity
+#define HH_h(h) h
+
+// Token pasting (use h, not h_)
+#define HH_H_(h, hh) h##hh
+#define HH_H(h, hh)  HH_H_(h, hh)
+
+// Stringize
+#define HH_hh_(h) #h
+#define HH_hh(h)  HH_hh_(h)
+
+#endif // #ifndef H
