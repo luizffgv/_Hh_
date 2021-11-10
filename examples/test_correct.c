8c7a3e81dd1ef41b78c70c5447367315f0d93fcb
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2021-11-10 16:59:32 -0300

    Testing workflow

---
+++ examples/test_correct.c
@@ -0,0 +1,9 @@
+#include <h.h>
+#include <stdio.h>
+
+H_H HH_H(HH_H(HH_H(_HHhh, _h), _Hhhh), _HHhH) hH_h H_ hH_H
+hH_hh
+    HH_H(HH_H(HH_H(_HHHH, _HhHhh), _HhhHH), _HhhHh) hH_h
+        HH_hh(HH_H(HH_H(HH_H(HH_H(_HHH_,_Hhh),_HhHH),_HhHH),_HHHh)) _h_HhHHhh _h_Hhhhhh HH_hh(HH_H(HH_H(HH_H(HH_H(_HhHHh_,_HHHh),_HhhhH),_HhHH),_HH)) _h_HhhhhH
+    hH_H hH_hH
+hH_HH
